db4449a4305bc8c44dc64d893ed033886efa1992
Felipe Costa <felipecostati@gmail.com>
2019-10-14 22:29:45 -0300

    Create translate-teste.js

---
+++ js/translate-teste.js
@@ -0,0 +1,33 @@
+function ChangeLang(a) {
+ var b, elemento = "";
+ if (document.createEvent) {
+ var c = document.createEvent("HTMLEvents");
+ c.initEvent("click", true, true)
+ }
+ if (a == 'pt') {
+ elemento = $(".goog-te-banner-frame:eq(0)").contents().find("button[id*='restore']")
+ } else {
+ switch (a) {
+ case 'en':
+ b = "ing";
+
+ }
+ elemento = $(".goog-te-menu-frame:eq(0)").contents().find("span:contains('" + b + "')");
+ }
+ if (elemento.length > 0) {
+ if (document.createEvent) {
+ elemento[0].dispatchEvent(c)
+ } else {
+ elemento[0].click()
+ }
+ }
+}
+function googleTranslateElementInit() {
+ new google.translate.TranslateElement({
+ pageLanguage: 'pt',
+ autoDisplay: false,
+ includedLanguages: 'es,en,',
+ layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+ },
+ 'google_translate_element');
+}
